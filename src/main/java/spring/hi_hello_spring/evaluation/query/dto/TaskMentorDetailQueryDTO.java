f5ecb7631408509f2761b89f50078556e44f8099
WhooGeek <rlarjswn0303@naver.com>
2024-12-05 02:01:58 +0900

    [FEAT] TaskMentorDetailQueryDTO 멘토 과제 상세조회 기능 구현

---
+++ src/main/java/spring/hi_hello_spring/evaluation/query/dto/TaskMentorDetailQueryDTO.java
@@ -0,0 +1,29 @@
+package spring.hi_hello_spring.evaluation.query.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class TaskMentorDetailQueryDTO {
+
+
+    private Long taskSeq;
+    private String taskType;
+    private Long templateSeq;
+    private String templateTitle;
+    private String templateUrl;
+    private String taskContent;
+    private String templateTaskRound;
+    private LocalDateTime templateEndAt;
+    private Long evalListSeq;
+    private String evalListContent;
+    private Integer evalListScore;
+
+}
