f2d52519cfd08d6335d6d3a1f885d9f474eaaf2e
minju <kminju0224@gmail.com>
2024-12-23 11:01:05 +0900

    [FIX] chat_room_seq 수정

--- src/main/java/spring/hi_hello_spring/config/WebSocketConfig.java
+++ src/main/java/spring/hi_hello_spring/config/WebSocketConfig.java
@@ -1,18 +1,25 @@
 package spring.hi_hello_spring.chatting;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
+@Slf4j
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // 클라이언트에서 연결할 WebSocket endpoint
-        registry.addEndpoint("/ws-stomp")
-                .setAllowedOrigins("*") ; // 허용할 특정 도메인으로 변경
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*") ; // 허용할 특정 도메인으로 변경
 //                .withSockJS();  // SockJS 지원
     }
 
@@ -29,5 +36,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setMessageSizeLimit(160 * 64 * 1024);
         registry.setSendTimeLimit(100 * 10000);
         registry.setSendBufferSizeLimit(3 * 512 * 1024);
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
